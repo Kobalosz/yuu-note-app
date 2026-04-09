6e21eca62852f8c31b58ee2033c6d5d8ecf13a6d
Kobalosz <imanuel.dartey@gmail.com>
2026-04-09 16:36:20 -0700

    Make entities/Note.java, Define Note constructor for project feature implementation.

---
+++ backend/src/main/java/com/example/backend/entities/Note.java
@@ -0,0 +1,40 @@
+import org.springframework.data
+
+
+public class Note {
+    private String id;
+    private String title;
+    private String content;
+    private String tag;
+
+    public Note(){}
+
+    public Note(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+            public String getId() {
+                return id;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public String getContent() {
+                return content;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public void setContent(String content) {
+                this.content = content;
+            }
+
+            public void setTag(String tag) {
+                this.tag = tag;
+            }
+        }
